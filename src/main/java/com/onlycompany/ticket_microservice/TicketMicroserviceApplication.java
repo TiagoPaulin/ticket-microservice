598bb9a6186e62362f67c2d30febe6e98505ccbe
TiagoPaulin <thiago.vmp15@gmail.com>
2024-09-10 15:56:20 -0300

    Project created

---
+++ src/main/java/com/onlycompany/ticket_microservice/TicketMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.onlycompany.ticket_microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketMicroserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/onlycompany/ticket_microservice/TicketMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onlycompany.ticket_microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
