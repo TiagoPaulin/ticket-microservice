ca3cb70a7e082c2a33d290aafafb6d561d8ca8ee
TiagoPaulin <thiago.vmp15@gmail.com>
2024-09-10 16:41:13 -0300

    Ticket entity

---
+++ src/main/java/com/onlycompany/ticket_microservice/models/Ticket.java
@@ -0,0 +1,84 @@
+package com.onlycompany.ticket_microservice.models;
+
+import jakarta.persistence.*;
+
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_ticket")
+public class Ticket {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Integer quantity;
+    private Float value;
+    private boolean verified;
+    private boolean preSale;
+
+    public Ticket() {}
+
+    public Ticket(Long id, Integer quantity, Float value, boolean verified, boolean preSale) {
+
+        this.id = id;
+        this.quantity = quantity;
+        this.value = value;
+        this.verified = verified;
+        this.preSale = preSale;
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public Float getValue() {
+        return value;
+    }
+
+    public void setValue(Float value) {
+        this.value = value;
+    }
+
+    public boolean isVerified() {
+        return verified;
+    }
+
+    public void setVerified(boolean verified) {
+        this.verified = verified;
+    }
+
+    public boolean isPreSale() {
+        return preSale;
+    }
+
+    public void setPreSale(boolean preSale) {
+        this.preSale = preSale;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Ticket ticket = (Ticket) o;
+        return Objects.equals(id, ticket.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+}
