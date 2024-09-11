985ef7205688271e431639d4691b90238060b080
TiagoPaulin <thiago.vmp15@gmail.com>
2024-09-11 13:55:45 -0300

    ticket findById

--- src/main/java/com/onlycompany/ticket_microservice/controllers/TicketController.java
+++ src/main/java/com/onlycompany/ticket_microservice/controllers/TicketController.java
@@ -1,7 +1,12 @@
 package com.onlycompany.ticket_microservice.controllers;
 
+import com.onlycompany.ticket_microservice.models.Event;
+import com.onlycompany.ticket_microservice.models.Ticket;
 import com.onlycompany.ticket_microservice.services.TicketService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,5 +16,14 @@ public class TicketController {
 
     @Autowired
     TicketService service;
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Ticket> findById(@PathVariable Long id) {
+
+        Ticket ticket = service.findById(id);
+
+        return ResponseEntity.ok().body(ticket);
+
+    }
 
 }
--- src/main/java/com/onlycompany/ticket_microservice/services/TicketService.java
+++ src/main/java/com/onlycompany/ticket_microservice/services/TicketService.java
@@ -1,13 +1,25 @@
 package com.onlycompany.ticket_microservice.services;
 
+import com.onlycompany.ticket_microservice.models.Event;
+import com.onlycompany.ticket_microservice.models.Ticket;
 import com.onlycompany.ticket_microservice.repositories.TicketRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class TicketService {
 
     @Autowired
     TicketRepository repository;
+
+    public Ticket findById(Long id) {
+
+        Optional<Ticket> ticket = repository.findById(id);
+
+        return ticket.get();
+
+    }
 
 }
