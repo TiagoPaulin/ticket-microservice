982d73171a368a2ca65c8982234ba349de871fcd
TiagoPaulin <thiago.vmp15@gmail.com>
2024-09-11 14:15:40 -0300

    EventType's CRUD

--- src/main/java/com/onlycompany/ticket_microservice/controllers/EventTypeController.java
+++ src/main/java/com/onlycompany/ticket_microservice/controllers/EventTypeController.java
@@ -1,9 +1,10 @@
 package com.onlycompany.ticket_microservice.controllers;
 
+import com.onlycompany.ticket_microservice.models.EventType;
 import com.onlycompany.ticket_microservice.services.EventTypeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "/type")
@@ -11,5 +12,41 @@ public class EventTypeController {
 
     @Autowired
     EventTypeService service;
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<EventType> findById(@PathVariable Long id) {
+
+        EventType type = service.findById(id);
+
+        return ResponseEntity.ok().body(type);
+
+    }
+
+    @PostMapping
+    public ResponseEntity<EventType> insert (@RequestBody EventType obj) {
+
+        obj = service.insert(obj);
+
+        return ResponseEntity.ok().body(obj);
+
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> delete (@PathVariable Long id) {
+
+        service.delete(id);
+
+        return ResponseEntity.noContent().build();
+
+    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<EventType> update (@PathVariable Long id, @RequestBody EventType obj) {
+
+        obj = service.update(id, obj);
+
+        return ResponseEntity.ok().body(obj);
+
+    }
 
 }
--- src/main/java/com/onlycompany/ticket_microservice/services/EventTypeService.java
+++ src/main/java/com/onlycompany/ticket_microservice/services/EventTypeService.java
@@ -1,13 +1,54 @@
 package com.onlycompany.ticket_microservice.services;
 
+import com.onlycompany.ticket_microservice.models.EventType;
 import com.onlycompany.ticket_microservice.repositories.EventTypeRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class EventTypeService {
 
     @Autowired
     EventTypeRepository repository;
+
+    public EventType findById(Long id) {
+
+        Optional<EventType> type = repository.findById(id);
+
+        return type.get();
+
+    }
+
+    public EventType insert(EventType obj) {
+
+        return repository.save(obj);
+
+    }
+
+    public void delete(Long id) {
+
+        repository.deleteById(id);
+
+    }
+
+    @Transactional
+    public EventType update(Long id, EventType obj) {
+
+        EventType type = repository.getReferenceById(id);
+
+        updateData(type, obj);
+
+        return repository.save(type);
+
+    }
+
+    private void updateData(EventType type, EventType obj) {
+
+        type.setDescription(obj.getDescription());
+
+    }
 
 }
